697c57f7735d614234789ba0be60af39ff66c743
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-12-30 19:11:26 +0100

    wasm

--- main.zig
+++ main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
+extern fn print(i32) void;
+
+export fn add(a: i32, b: i32) void {
+    print(a + b);
+}
+
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
     defer list.deinit();
